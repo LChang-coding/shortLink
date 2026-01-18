b5fc69f52fbad92700c56f2afbc2ee2885bc841e
LChang-coding <jwjwcwcw18714610648@gmail.com>
2026-01-18 21:27:57 +0800

    完成一些枚举错误码的创建

--- admin/src/main/java/com/nageoffer/shortlink/admin/controller/UserController.java
+++ admin/src/main/java/com/nageoffer/shortlink/admin/controller/UserController.java
@@ -17,10 +17,11 @@
 
 package com.nageoffer.shortlink.admin.controller;
 
+import com.nageoffer.shortlink.admin.common.convention.result.Result;
+import com.nageoffer.shortlink.admin.common.convention.result.Results;
 import com.nageoffer.shortlink.admin.dto.resp.UserRespDTO;
 import com.nageoffer.shortlink.admin.service.UserService;
 import lombok.RequiredArgsConstructor;
-import org.apache.ibatis.annotations.Results;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -37,8 +38,12 @@ public class UserController {
     /**
      * 根据用户名查询用户信息
      */
+    /**
+     * 根据用户名查询用户信息
+     */
     @GetMapping("/api/short-link/admin/v1/user/{username}")
-    public UserRespDTO getUserByUsername(@PathVariable("username") String username) {
-        return userService.getUserByUsername(username);
+    public Result<UserRespDTO> getUserByUsername(@PathVariable("username") String username) {
+        return Results.success(userService.getUserByUsername(username));
     }
+
 }
