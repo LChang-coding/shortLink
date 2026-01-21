aeaf7e72faddb821bfa8589cbc1220bd47210939
LChang-coding <jwjwcwcw18714610648@gmail.com>
2026-01-21 14:14:28 +0800

    短链接后管调用中台创建和分页查询请求

---
+++ admin/src/main/java/com/nageoffer/shortlink/admin/remote/dto/ShortLinkRemoteService.java
@@ -0,0 +1,47 @@
+package com.nageoffer.shortlink.admin.remote.dto;
+
+
+import cn.hutool.http.HttpUtil;
+import com.alibaba.fastjson2.JSON;
+import com.alibaba.fastjson2.JSONObject;
+import com.alibaba.fastjson2.TypeReference;
+import com.baomidou.mybatisplus.core.metadata.IPage;
+import com.nageoffer.shortlink.admin.common.convention.result.Result;
+import com.nageoffer.shortlink.admin.remote.dto.req.ShortLinkCreateReqDTO;
+import com.nageoffer.shortlink.admin.remote.dto.req.ShortLinkPageReqDTO;
+import com.nageoffer.shortlink.admin.remote.dto.resp.ShortLinkCreateRespDTO;
+import com.nageoffer.shortlink.admin.remote.dto.resp.ShortLinkPageRespDTO;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * 短链接中台远程服务接口
+ */
+public interface ShortLinkRemoteService {
+
+    default ShortLinkCreateRespDTO createShortLink(ShortLinkCreateReqDTO requestParam) {
+        String resultBodyStr = HttpUtil.post("http://127.0.0.1:8001/api/short-link/v1/create", JSON.toJSONString(requestParam));
+        Result<JSONObject> tempResult =
+                JSON.parseObject(resultBodyStr, new TypeReference<Result<JSONObject>>() {});
+
+        JSONObject data = tempResult == null ? null : tempResult.getData();
+        if (data == null) {
+            return null;
+        }
+
+        // 用 JSONObject 再转一次，避免因为泛型/类型信息导致的映射问题，并便于你断点查看 data 的真实键名
+        return data.toJavaObject(ShortLinkCreateRespDTO.class);
+    }
+
+
+
+    default  public Result<IPage<ShortLinkPageRespDTO>> pageShortLink(ShortLinkPageReqDTO requestParam){
+        Map<String, Object> requestMap = new HashMap<>();
+        requestMap.put("gid",requestParam.getGid());
+        requestMap.put("current",requestParam.getCurrent());
+        requestMap.put("size",requestParam.getSize());
+        String resultPageStr= HttpUtil.get("http://127.0.0.1:8001/api/short-link/v1/page",requestMap);
+        return JSON.parseObject(resultPageStr,new TypeReference<>(){});
+    }
+}
