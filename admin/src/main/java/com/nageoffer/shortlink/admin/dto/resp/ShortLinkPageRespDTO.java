d7e0be010e41d5eefe09dd3aad988d4e23f1efa9
LChang-coding <jwjwcwcw18714610648@gmail.com>
2026-01-26 14:28:30 +0800

    今日监控表的分表处理以及完善新增时短链接的初始化

--- admin/src/main/java/com/nageoffer/shortlink/admin/dto/resp/ShortLinkPageRespDTO.java
+++ admin/src/main/java/com/nageoffer/shortlink/admin/dto/resp/ShortLinkPageRespDTO.java
@@ -1,20 +1,3 @@
-/*
- * Licensed to the Apache Software Foundation (ASF) under one or more
- * contributor license agreements.  See the NOTICE file distributed with
- * this work for additional information regarding copyright ownership.
- * The ASF licenses this file to You under the Apache License, Version 2.0
- * (the "License"); you may not use this file except in compliance with
- * the License.  You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 package com.nageoffer.shortlink.admin.dto.resp;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
@@ -65,11 +48,6 @@ public class ShortLinkPageRespDTO {
     private Integer validDateType;
 
     /**
-     * 启用标识 0：启用 1：未启用
-     */
-    private Integer enableStatus;
-
-    /**
      * 有效期
      */
     @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "GMT+8")
@@ -91,6 +69,7 @@ public class ShortLinkPageRespDTO {
      */
     private String favicon;
 
+
     /**
      * 历史PV
      */
@@ -99,7 +78,7 @@ public class ShortLinkPageRespDTO {
     /**
      * 今日PV
      */
-    private Integer todayPv;
+    private Integer toDayPv;
 
     /**
      * 历史UV
@@ -109,15 +88,15 @@ public class ShortLinkPageRespDTO {
     /**
      * 今日UV
      */
-    private Integer todayUv;
+    private Integer toDayUv;
 
     /**
      * 历史UIP
      */
-    private Integer totalUip;
+    private Integer totalUIp;
 
     /**
      * 今日UIP
      */
-    private Integer todayUip;
+    private Integer toDayUIp;
 }
--- project/src/main/java/com/nageoffer/shortlink/project/dao/entity/ShortLinkDO.java
+++ project/src/main/java/com/nageoffer/shortlink/project/dao/entity/ShortLinkDO.java
@@ -1,20 +1,3 @@
-/*
- * Licensed to the Apache Software Foundation (ASF) under one or more
- * contributor license agreements.  See the NOTICE file distributed with
- * this work for additional information regarding copyright ownership.
- * The ASF licenses this file to You under the Apache License, Version 2.0
- * (the "License"); you may not use this file except in compliance with
- * the License.  You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 package com.nageoffer.shortlink.project.dao.entity;
 
 import com.baomidou.mybatisplus.annotation.TableField;
@@ -118,27 +101,4 @@ public class ShortLinkDO extends BaseDO {
      * 历史UIP
      */
     private Integer totalUip;
-
-    /**
-     * 今日PV
-     */
-    @TableField(exist = false)
-    private Integer todayPv;
-
-    /**
-     * 今日UV
-     */
-    @TableField(exist = false)
-    private Integer todayUv;
-
-    /**
-     * 今日UIP
-     */
-    @TableField(exist = false)
-    private Integer todayUip;
-
-    /**
-     * 删除时间
-     */
-    private Long delTime;
 }
